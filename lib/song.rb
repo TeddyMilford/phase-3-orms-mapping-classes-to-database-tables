2387bf1aacf02466c5cfbc8942187503190eeee5
Teddy Milford <teddy.milford@gmail.com>
2022-06-29 13:26:41 -0600

    commit tree e4b1a1ed6105967352daea665548c5e74e4cd46b

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,4 @@
 class Song
-
   attr_accessor :name, :album
 
   def initialize(name:, album:)
@@ -7,4 +6,14 @@ class Song
     @album = album
   end
 
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        album TEXT
+      )
+      SQL
+    DB[:conn].execute(sql)
+  end
 end
